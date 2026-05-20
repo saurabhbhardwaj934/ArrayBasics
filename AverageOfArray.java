c77d8c06496543839407b377e0a66cf5b0094101
saurabhbhardwaj934 <saurabhbhardwaj067@gmail.com>
2026-05-20 14:52:35 +0530

    Added average of array question

---
+++ AverageOfArray.java
@@ -0,0 +1,13 @@
+public class AverageOfArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        double average = sum / arr.length;
+        System.out.println("The average of the array is: " + average);
+    }
+}
